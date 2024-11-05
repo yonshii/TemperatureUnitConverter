ad890525189e1762e551e7a64fdbeee363d74d65
yonshii <dkaranets2@gmail.com>
2024-11-05 15:29:17 +0100

    Added Celsius to Fahrenheit conversion

--- src/Main.java
+++ src/Main.java
@@ -5,4 +5,7 @@ public class Main {
     public static double fahrenheitToCelsius(double fahrenheit) {
         return (fahrenheit - 32) * 5 / 9;
     }
+    public static double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9 / 5) + 32;
+    }
 }
