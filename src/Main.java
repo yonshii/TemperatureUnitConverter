c46e2918706375239b12e6098f54cdaf67cdaf85
yonshii <dkaranets2@gmail.com>
2024-11-05 15:27:00 +0100

    Added Fahrenheit to Celsius conversion

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Temperature unit converter");
     }
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
+    }
 }
